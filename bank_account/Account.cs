c2e530074447b843fb74612bf231c89965ed4e19
KayqueS <kayque.sanmartin65@gmail.com>
2021-03-10 14:42:50 -0300

    Add project files.

---
+++ bank_account/Account.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bank_account
+{
+    class Account
+    {
+        public int AccountNumber { get; private set; }
+        public string OwnerAccount { get; set; }
+        public double Balance { get; private set; }
+
+        public Account(int accountNumber, string ownerAccount)
+        {
+            AccountNumber = accountNumber;
+            OwnerAccount = ownerAccount;
+        }
+
+        public Account(int accountNumber, string ownerAccount, double balance) : this(accountNumber, ownerAccount)
+        {
+            Balance = balance;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+
+        public void Withdraw(double amount)
+        {
+            Balance -= amount + 5.0;
+        }
+
+
+
+        public override string ToString()
+        {
+            return $"Account {AccountNumber}, Owner: {OwnerAccount}, Balance: $ {Balance:F2}";
+        }
+    }
+}
---
+++ bank_account/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace bank_account
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account bank;
+
+            Console.Write("Enter the account number: ");
+            int accountNumber = int.Parse(Console.ReadLine());
+
+            Console.Write("Sign in with the account holder: ");
+            string ownerAccount = Console.ReadLine();
+
+            Console.Write("There will be an initial deposit (Y/N)? ");
+            char answer = char.Parse(Console.ReadLine());
+            if (answer == 'Y' || answer == 'y')
+            {
+                Console.Write("Enter the initial deposit amount: $");
+                double deposit = double.Parse(Console.ReadLine());
+                bank = new Account(accountNumber, ownerAccount, deposit);
+            }
+            else
+            {
+                bank = new Account(accountNumber, ownerAccount);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Account Data:");
+            Console.WriteLine(bank);
+
+            Console.WriteLine();
+            Console.Write("Enter a deposit amount: $");
+            double amount = double.Parse(Console.ReadLine());
+            bank.Deposit(amount);
+            Console.WriteLine("Updated Account Data:");
+            Console.WriteLine(bank);
+
+            Console.WriteLine();
+            Console.Write("Enter a withdrawal amount: $");
+            amount = double.Parse(Console.ReadLine());
+            bank.Withdraw(amount);
+            Console.WriteLine("Updated Account Data:");
+            Console.WriteLine(bank);
+
+        }
+    }
+}
